43954018483dde609a6cb885b3d8b4a58d6ad6dd
Mohamed Ait Elkadi <mait-elk@e3r3p15.1337.ma>
2023-12-04 15:03:58 +0100

    done

---
+++ main.c
@@ -0,0 +1,44 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mait-elk <mait-elk@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/12/04 10:59:37 by mait-elk          #+#    #+#             */
+/*   Updated: 2023/12/04 15:03:51 by mait-elk         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+#include <stdio.h>
+#include <fcntl.h>
+
+char	betafunc(unsigned int pos, char c)
+{
+	return (ft_toupper(c));
+}
+
+int	main(int argc, char *argv[])
+{
+	char	*str;
+	int		len_chars;
+	int		len_lines;
+	int		input_fd;
+
+	len_lines = 0;
+	input_fd = open(argv[1], O_RDONLY);
+	if (argc < 2 || read(input_fd, 0, 0) == -1)
+		return (close(input_fd), ft_printf("INVALID FILE"));
+	while ((str = get_next_line(input_fd)))
+	{
+		str = ft_strmapi(str, &betafunc);
+		len_chars += _nsx_strlen(str);
+		len_lines++;
+		ft_putstr_fd(str, 1);
+		free(str);
+		str = 0;
+	}
+	ft_printf("\n\n\t\e[1;42m(OUTPUT LENGTH : %d)\e[0;0m", len_chars);
+	ft_printf("\n\n\t\e[1;42m(OUTPUT LINES : %d)", len_lines);
+}
